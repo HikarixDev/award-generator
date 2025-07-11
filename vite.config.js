99b082dd725428ea03caba524f04687c2c96ac0a
HikarixDev <139475760+HikarixDev@users.noreply.github.com>
2025-07-11 16:35:36 +0200

    Asd4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
+// vite.config.js
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+    base: "/award-generator/", // this is critical for GitHub Pages!
     plugins: [react()],
-    base: "/award-generator/", // match your repo name exactly
     resolve: {
         alias: {
-            "@": "/src"
+            "@/components": "/src/components"
         }
     }
 })
